793e1084fec0c01efa377abd341198e16e5a14ee
Bruno Malhano <malhano94@hotmail.com>
2023-10-19 01:19:12 -0300

    first commit

---
+++ MyTasks/js/firebase.js
@@ -0,0 +1,14 @@
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+  apiKey: "{ApiKey}",
+  authDomain: "{authDomain}",
+  databaseURL:"{databaseURL}",
+  projectId: "{projectId}",
+  storageBucket: "{storageBucket}",
+  messagingSenderId: "{messagingSenderId}",
+  appId: "{appId}",
+};
+
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
